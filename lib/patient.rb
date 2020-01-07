731dd8da2cd38bd96ff7704631c7968ffaf0b8a1
Luana Faria Lane <luanasfff@gmail.com>
2020-01-07 02:55:03 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,4 +1,30 @@
 class Patient
   
   
- 
+ attr_accessor :name, :appointment, :doctor
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+    #  binding.pry
+  end
+
+  def doctors
+    appointments.collect {|appointment| appointment.doctor}
+  end
+
+
+end
